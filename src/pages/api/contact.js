8b4ae7ecef54e836003eb6e4d675fce9ab455bb4
puwadonnue <puwadoncnue@gmail.com>
2024-06-17 10:28:12 +0700

    add secret

--- src/pages/api/contact.js
+++ src/pages/api/contact.js
@@ -28,7 +28,7 @@ const reqBodySchema = Joi.object({
 		.required(),
 });
 
-const TURNSTILE_SECRET_KEY = process.env.TURNSTILE_SECRET_KEY;
+const TURNSTILE_SECRET_KEY = "0x4AAAAAAAcqiPSpQq3gA9i2QNqVl0u6_JQ";
 const TURNSTILE_VERIFY_URL =
 	'https://challenges.cloudflare.com/turnstile/v0/siteverify';
 
