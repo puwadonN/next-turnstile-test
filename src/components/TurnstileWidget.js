a6300e52558e72f446f69896e47faf35a37276ff
puwadonnue <puwadoncnue@gmail.com>
2024-06-18 09:39:56 +0700

    feat: add props parameter action and cdata to turnstile

--- src/components/TurnstileWidget.js
+++ src/components/TurnstileWidget.js
@@ -6,6 +6,13 @@ const TurnstileWidget = React.forwardRef(function TurnstileWidget(props, ref) {
 		<Turnstile
 			ref={ref}
 			siteKey={process.env.NEXT_PUBLIC_TURNSTILE_SITE_KEY}
+			options={{
+				action: 'submit-form',
+				cData: 'access token',
+				theme: 'light',
+				// size: 'compact',
+				// language: 'fr',
+			  }}		
 			{...props}
 		/>
 	);
